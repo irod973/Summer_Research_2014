3db42f9878560376a6cd2f4e5023b5916418f156
irod973 <irving.rodriguez973@gmail.com>
2014-09-02 12:36:01 -0700

    updated drawGaussianMixture

--- Galsim_Work/Gaussian_Work/drawGaussianMixture.py
+++ Galsim_Work/Gaussian_Work/drawGaussianMixture.py
@@ -3,149 +3,85 @@ import galsim
 import lmfit as lm
 import numpy as np
 import matplotlib.pyplot as pl
+import gaussianMixtureLib as lib
 
 #Irving Rodriguez
 #
 #Draws a Gaussian mixture composed of 2 Gaussians, draws a single Gaussian fit, and draws the residual between the mixture and the fit.
 
-
 #-------------------------------------------------------------------------
-#Functions--------------------------------------------------------------------
-#-------------------------------------------------------------------------
-
-
-def drawFit(params, pixelScale=.2, stampSize=100, psf=False):
-	flux = params['fitFlux'].value
-	hlr = params['fitHLR'].value
-	centX = params['fitCentX'].value
-	centY = params['fitCentY'].value
-	e1 = params['fite1'].value
-	e2 = params['fite2'].value
-
-	fit = galsim.Gaussian(flux=flux, half_light_radius=hlr)
-	fit = fit.shear(e1=e1, e2=e2)
-	fit = fit.shift(dx=centX, dy=centY)
-	if psf == True:
-		psf = galsim.Moffat(beta=3, fwhm=.6)
-		fit = galsim.Convolve([fit, psf])
-		fitImage = galsim.ImageD(stampSize, stampSize, scale=pixelScale)
-		fitImage = fit.draw(image=fitImage)
-		return fitImage
-	fitImage = galsim.ImageD(stampSize, stampSize, scale=pixelScale)
-	fitImage = fit.draw(image=fitImage)
-	return fitImage
-
-#Draw mixture using FFT
-def drawMixture(domParams, contParams, skyMap, pixelScale=.2, stampSize=100,  sky=False, psf=False):
-	domGal = galsim.Gaussian(flux=domParams['flux'].value*domParams['frac'].value, half_light_radius=domParams['HLR'].value)
-	contGal = galsim.Gaussian(flux=contParams['flux'].value*contParams['frac'].value, half_light_radius=contParams['HLR'].value)
-	contGal = contGal.shift(dx=contParams['centX'].value, dy=contParams['centY'].value)
-	mix =  domGal + contGal
-	if psf==True:
-		psf = galsim.Moffat(beta=3, fwhm=.6)
-		mix = galsim.Convolve([mix, psf])
-		mixImage = galsim.ImageD(stampSize, stampSize, scale=pixelScale)
-		mixImage = mix.drawImage(image=mixImage, method='fft')
-		if sky==True:
-			mixImage.addNoise(galsim.PoissonNoise(rng=skyMap.get('rng'), sky_level=skyMap.get('meanSky')*skyMap.get('expTime'))) #skylevel is counts/sec/pixel times 30 sec exposure time
-		gals = [domGal, contGal.original, mix]
-		return  gals, mixImage
-	mixImage = galsim.ImageD(stampSize, stampSize, scale=pixelScale)
-	mixImage = mix.drawImage(image=mixImage, method='fft')
-	if sky==True:
-		mixImage.addNoise(galsim.PoissonNoise(rng=skyMap.get('rng'), sky_level=skyMap.get('')*skyMap.get('expTime'))) #skylevel is counts/sec/pixel multiplied by full 10-year LSST band exposure time
-	gals = [domGal, contGal.original, mix]
-	return gals, mixImage
-
-#Compute the chi-squared residual between the parameter image and the mixture image, using errors from the average sky level over a full stack
-def residual(params, mixIm, skyMap, pixelScale=.2, stampSize=100):
-	fitIm = drawFit(params, pixelScale, stampSize)
-	return (mixIm - fitIm).array.ravel()**2/(skyMap.get('meanSky')*skyMap.get('expTime') + fitIm.array.ravel())
-
-#Sum of flux from pixels above half the average sky level
-def fluxAboveThreshold(image, skyMap):
-	fluxAboveThresh = 0
-	threshold = .5*(skyMap.get('meanSky')*skyMap.get('expTime'))**.5
-	for elem in image.array.ravel():
-		if elem >= threshold:
-			fluxAboveThresh += elem
-	return fluxAboveThresh
-
-
-#-------------------------------------------------------------------------
-#Body---------------------------------------------------------------------
+#---------------------------------------------------------------------
 #-------------------------------------------------------------------------
 
-#Parameters for Mixture
-DOMINANT_FLUX = 7.5e5
+###############################Define constants
+#Galaxy parameters
+DOMINANT_FLUX = 1.e6
 DOMINANT_HALF_LIGHT_RADIUS = 1  #arcsec
 DOMINANT_FLUX_FRACTION = .5 #Fractional flux of dominant galaxy, so contFrac = 1-domFrac
+DOMINANT_E1 = 0
+DOMINANT_E2 = 0
+DOMINANT_CENTROID = (0,0)
 CONTAMINANT_FLUX = DOMINANT_FLUX
 CONTAMINANT_HALF_LIGHT_RADIUS = DOMINANT_HALF_LIGHT_RADIUS
 CONTAMINANT_FLUX_FRACTION = 1 - DOMINANT_FLUX_FRACTION
+CONTAMINANT_E1 = 0
+CONTAMINANT_E2 = 0
 PIXEL_SCALE = .2 #arcsec/pixel
 STAMP_SIZE = 100 #pixels
-SEPARATION = 10 * PIXEL_SCALE #pixels*pixelscale = "
+SEPARATION = 5 * PIXEL_SCALE #pixels*pixelscale = "
 PHI = 0 #angle between major axis and real x-axis
 dx = SEPARATION*np.cos(PHI)
 dy = SEPARATION*np.sin(PHI)
-PSF = True
-SKY = True #booleans
+PSF=True
+SKY=True
 
-#Parameters for sky
+#Sky parameters
 MEAN_SKY_BACKGROUND = 26.83 #counts/sec/pixel, from red band (David Kirkby notes)
 EXPOSURE_TIME = 6900 #sec, from LSST
 rng = galsim.BaseDeviate(1)
 skyMap = {'meanSky':MEAN_SKY_BACKGROUND, 'expTime':EXPOSURE_TIME, 'rng':rng}
 
-
-#Mixture Parameters
+#Initialize parameter objects
 domParams = lm.Parameters()
 domParams.add('flux', value=DOMINANT_FLUX)
 domParams.add('HLR', value=DOMINANT_HALF_LIGHT_RADIUS)
-domParams.add('centX', value=0)
-domParams.add('centY', value=0)
-domParams.add('frac', value=DOMINANT_FLUX_FRACTION)
+domParams.add('centX', value=DOMINANT_CENTROID[0])
+domParams.add('centY', value=DOMINANT_CENTROID[1])
+domParams.add('e1', value=DOMINANT_E1)
+domParams.add('e2', value=DOMINANT_E2)
+domParams.add('fluxFrac', value=DOMINANT_FLUX_FRACTION)
 
 contParams = lm.Parameters()
 contParams.add('flux', value=CONTAMINANT_FLUX)
 contParams.add('HLR', value=CONTAMINANT_HALF_LIGHT_RADIUS)
 contParams.add('centX', value=dx)
 contParams.add('centY', value=dy)
-contParams.add('frac', value=CONTAMINANT_FLUX_FRACTION)
+contParams.add('e1', value=CONTAMINANT_E1)
+contParams.add('e2', value=CONTAMINANT_E2)
+contParams.add('fluxFrac', value=CONTAMINANT_FLUX_FRACTION)
 
-#If uncertainties for fit parameters are desired, use these values accordingly, else the fitter will not find enough points away from min ChiSquared to estimate uncertainties
+#Initialize fit parameters
 params = lm.Parameters()
-params.add('fitFlux', value=500000)
-params.add('fitHLR', value=2)
-params.add('fitCentX', value=2)
-params.add('fitCentY', value=0)
-params.add('fite1', value=0, min=-1, max=1)
-params.add('fite2', value=0, min=-1, max=1)
-
-#If uncertainties for fit parameters are not desired, use these values to fit faster
-# params = lm.Parameters()
-# params.add('fitFlux', value=mixZeroMom)
-# params.add('fitHLR', value=2)
-# params.add('fitCentX', value=mixFirstMom[0])
-# params.add('fitCentY', value=mixFirstMom[1])
-# params.add('fite1', value=e1A)
-# params.add('fite2', value=e2A)
+params.add('fitFlux', value=domParams['flux'].value)
+params.add('fitHLR', value=domParams['HLR'].value)
+params.add('fitCentX', value=domParams['centX'].value)
+params.add('fitCentY', value=domParams['centY'].value)
+params.add('fite1', value=domParams['e1'].value, min=-1, max=1)
+params.add('fite2', value=domParams['e2'].value, min=-1, max=1)
 
 #Create mixture
-gals, mixIm = drawMixture(domParams, contParams, skyMap, psf=PSF, sky=SKY)
+gals, mixIm = lib.drawMixture(domParams, contParams, skyMap, psf=PSF, sky=SKY)
+
 #Minimize
-out = lm.minimize(residual, params, args=[mixIm, skyMap])
+if PSF==True:
+	out = lm.minimize(lib.residualPSF, params, args=[mixIm, skyMap])
+else:
+	out = lm.minimize(lib.residual, params, args=[mixIm, skyMap])
 print 'The minimum Chi-Squared is: ', out.chisqr
 lm.report_errors(params)
 
 #Draw best fit
-fitIm = drawFit(params, psf=PSF)
-
-fig = pl.figure()
-ax11 = fig.add_subplot(111)
-ax11.imshow(mixIm.array, origin='lower')
-pl.show()
+fitIm = lib.drawFit(params, psf=PSF)
 
 ###Plots
 fig = pl.figure()
@@ -173,30 +109,32 @@ ax13.set_title('Residual', fontsize=18)
 pl.colorbar(c3, shrink=.5)
 
 #Get the percentage of flux above sky level
-fluxRange = np.linspace(1, 5.e6, 1000)
-percentThresh = []
-for flux in fluxRange:
-	domParams['flux'].value = flux
-	contParams['flux'].value = flux
-	gals, image = drawMixture(domParams, contParams, skyMap, sky=False)
-	fluxAbove = fluxAboveThreshold(image, skyMap)
-	percentThresh.append(fluxAbove/(flux))
+# fluxRange = np.linspace(1, 5.e6, 1000)
+# percentThresh = []
+# for flux in fluxRange:
+# 	domParams['flux'].value = flux
+# 	contParams['flux'].value = flux
+# 	gals, image = lib.drawMixture(domParams, contParams, skyMap, sky=False)
+# 	fluxAbove = fluxAboveThreshold(image, skyMap)
+# 	percentThresh.append(fluxAbove/(flux))
 
-#Plot percent flux above threshold
-fig = pl.figure(2)
-ax21 = fig.add_subplot(111)
-ax21.plot(fluxRange, percentThresh, '.')
-ax21.axhline(y=.95, label='95%')
-ax21.set_title('Percent Flux Above Sky (sum over flux of pixels) vs. Total Mixture Flux')
-ax21.legend(loc=7, prop={'size':12})
+# #Plot percent flux above threshold
+# fig = pl.figure(2)
+# ax21 = fig.add_subplot(111)
+# ax21.plot(fluxRange, percentThresh, '.')
+# ax21.axhline(y=.95, label='95%')
+# ax21.set_title('Percent Flux Above Sky (sum over flux of pixels) vs. Total Mixture Flux')
+# ax21.legend(loc=7, prop={'size':12})
 
+#Calculate Signal to Noise Ratio
 threshold = .5*(skyMap.get('meanSky')*skyMap.get('expTime'))**.5
 mask = mixIm.array>=threshold
 weight = mixIm.array
 snr = (mixIm.array*mask).sum() / np.sqrt((skyMap.get('meanSky')*skyMap.get('expTime')*mask).sum())
 wsnr = (mixIm.array*weight*mask).sum() / np.sqrt((weight*weight*skyMap.get('meanSky')*skyMap.get('expTime')*mask).sum())
-print 'Weighted: ', wsnr
+print 'Weighted SNR: ', wsnr
 
+#Show mask of pixels above threshold
 fig = pl.figure()
 ax01 = fig.add_subplot(111)
 im01 = ax01.imshow(mask)
