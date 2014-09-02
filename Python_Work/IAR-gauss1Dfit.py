fd9979acf6c9aab47b35b27f0b357dca84e4bbfc
irod973 <irving.rodriguez973@gmail.com>
2014-09-02 15:53:10 -0700

    revisions after Mandeep meeting

--- Python_Work/IAR-gauss1Dfit.py
+++ Python_Work/IAR-gauss1Dfit.py
@@ -3,21 +3,26 @@ from scipy import optimize as opt
 from scipy import stats as st
 from matplotlib import pyplot as pl
 
+#Irving Rodriguez
+#
+#This file creates a mixture of two 1D Gaussians and fits using one profile. Will do an analytic and binned mixture.
+#Summer 2014
+
 def main():
-    PA = .5
-    MEAN = 5
-    SIGMA = 1
-    AMP = 60
+    FLUX_FRACTION = .5
+    MEAN = 0
+    SIGMA = 3
+    AMP = 30
     
-    NOISE_MEAN = 0
-    NOISE_SIGMA = 1
-    NOISE_AMP = 25
+    NOISE_MEAN = 5
+    NOISE_SIGMA = 3
+    NOISE_AMP = 30
     
     #Fits a Gaussian to the sum of two analytic Gaussians
-    #analyticSumFit(MEAN, SIGMA, AMP, NOISE_MEAN, NOISE_SIGMA, NOISE_AMP)
+    analyticSumFit(MEAN, SIGMA, AMP, NOISE_MEAN, NOISE_SIGMA, NOISE_AMP)
     
     #Fits a Gaussian to the sum of two Gaussian histograms
-    binSumFit(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, PA)
+    binSumFit(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, FLUX_FRACTION)
 
 def gaussianpdf(x, u, s, amp=1):
     return amp*np.exp(-(x-u)**2/(2.0*s**2))#/(np.sqrt(2*np.pi))
@@ -26,45 +31,52 @@ def analyticSumFit(mean, sig, amp, nmean, nsigma, namp):
     dataWeight = .5
     objWeight = 1 - dataWeight
     
-    x = np.linspace(-10,10, 10000)
+    x = np.linspace(-10,15, 1000)
+    #Create two analytic functions
     data = dataWeight*gaussianpdf(x, mean, sig, amp)
     obj = objWeight*gaussianpdf(x, nmean, nsigma, amp)
     dataSum = data + obj
        
+    #Fit to the sum of the two functions
     stats, error = opt.curve_fit(gaussianpdf, x, dataSum, p0=[0, 1, 30])
-    sumFit = gaussianpdf(new, stats[0], stats[1], stats[2])
+    #Draw fit using returned parameters from curve_fit
+    sumFit = gaussianpdf(x, stats[0], stats[1], stats[2])
     
+    #Plot
     fig = pl.figure()
     params = [mean, sig, amp, nmean, nsigma, namp]
     paramStr = "Params (mean1, sig1, amp1, mean2, sig2, amp2): " + ', '.join([str(param) for param in params])
     statStr = 'Stats of Sum Fit(mean, sigma): ' + ', '.join([str(round(stat, 2)) for stat in stats])
     titleStr = 'Sum of two 1-D Gaussians\n' + paramStr + '\n' + statStr
-    fig.suptitle(titleStr)
+    #fig.suptitle(titleStr)
     ax = fig.add_subplot(111)
-    ax.plot(x, data, color='b', label = 'Original')
-    ax.plot(x, obj, color='g', label = 'Second Gaussian')
-    ax.plot(x, dataSum, color='r', label='Sum')
-    ax.plot(x, sumFit, color='k', label = 'Sum Fit')
-    ax.set_xlim(-35, 35)
-    pl.legend(loc=2)
+    ax.plot(x, data, color='b', label = 'Dominant Galaxy', linewidth=3)
+    ax.plot(x, obj, color='g', label = 'Contaminant Galaxy', linewidth=3)
+    #ax.plot(x, dataSum, color='r', label='Sum')
+    ax.plot(x, sumFit,'k--', label = 'Single Galaxy Fit', linewidth=3)
+    ax.set_xlim(-10, 15)
+    ax.set_ylim(0, 40)
+    ax.set_xlabel('x', fontsize=18)
+    ax.set_ylabel('Flux', fontsize=18)
+    pl.legend(loc=9, prop={'size':18})
     pl.show()
     
-def binSumFit(u, s, nu, ns, pa = .5):
+def binSumFit(mean, sigma, contaminantMean, contaminantSigma, dominantFrac = .5):
     samples = 10000
     bins = 150
-    pb = 1 - pa
-    dataSamples = pa*samples
-    objSamples = pb*samples
+    contaminantFrac = 1 - dominantFrac
+    dataSamples = dominantFrac*samples
+    objSamples = contaminantFrac*samples
     
     #Set up bin space
-    xMin = np.mean([u, nu]) - (bins/10)*s
-    xMax = np.mean([u, nu]) + (bins/10)*s
+    xMin = np.mean([mean, contaminantMean]) - (bins/10)*sigma
+    xMax = np.mean([mean, contaminantMean]) + (bins/10)*sigma
     x = np.linspace(xMin, xMax, bins)
     
-    #Draw samples for data and for undetected object
+    #Draw samples for data and for meanndetected object
     np.random.seed(0)
-    data = np.random.normal(u, s, dataSamples)
-    obj = np.random.normal(nu, ns, objSamples)
+    data = np.random.normal(mean, sigma, dataSamples)
+    obj = np.random.normal(contaminantMean, contaminantSigma, objSamples)
     binSum = np.concatenate((data, obj))
     
     #Create histograms for data and object
@@ -74,17 +86,17 @@ def binSumFit(u, s, nu, ns, pa = .5):
     sumEdgCent = (sumEdg[:-1] + sumEdg[1:])/2
     
     #Extract the paramaters of the sum fit
-    p=[pa*u+pb*nu, s, max(sumHist)]
+    p=[dominantFrac*mean+contaminantFrac*contaminantMean, sigma, max(sumHist)]
     sumStats, sumErr = opt.curve_fit(gaussianpdf, sumEdgCent, sumHist, p0=p)
     #Create fit
     sumFit = gaussianpdf(sumEdgCent, sumStats[0], sumStats[1], sumStats[2])
 
     #Title Strings
-    params = [u, s, nu, ns]
+    params = [mean, sigma, contaminantMean, contaminantSigma]
     statsFit = [sumStats[0], sumStats[1], sumStats[2]]
     paramStr = "Params (meanData, sigData, meanObj, sigObj): " + ', '.join([str(param) for param in params])
     statStr = 'Stats of Sum Fit(mean, sigma, amp): ' + ', '.join([str(np.round(stat, 2)) for stat in statsFit])
-    titleStr = 'Gaussian Mixture, '+ str(pa*100) + '% Red with '+str(pb*100)+'% Green\n' + paramStr + '\n' + statStr    
+    titleStr = 'Gaussian Mixture, '+ str(dominantFrac*100) + '% Red with '+str(contaminantFrac*100)+'% Green\n' + paramStr + '\n' + statStr    
  
     #Plotting our results
     fig = pl.figure()
--- Python_Work/IAR-gauss1Dmix-VarStudy.py
+++ Python_Work/IAR-gauss1Dmix-VarStudy.py
@@ -6,167 +6,164 @@ from matplotlib import pyplot as pl
 from matplotlib.widgets import Slider, Button, RadioButtons
 
 #A study of the behavior of Variance in a 1D Gaussian Mixture (binned)
+#Will Show 5 Plots:
+#1. Variance of Best Fitvs. Separation
+#2. Mean of Fit vs. Separation
+#3. Variance of Fit vs. Flux Fraction
+#4. Gaussian Mixture (2 profiles)
+#5. Chi-Squared Colormap
 
 def main():
-    #When keeping parameters fixed
-    PA = .5
+    #Fixed parameters
+    DOMINANT_FLUX_FRACTION = .5
     MEAN = 0
     SIGMA = 1   
     NOISE_MEAN = 2
     NOISE_SIGMA = 1
-    #When moving through parameter space
-    diff = abs(MEAN-NOISE_MEAN)
-    nmean = np.linspace(MEAN-abs(diff),MEAN+abs(diff), 21)
-    p = np.linspace(.5, .99, 50)
 
+    #Varying parameters
+    sep = abs(MEAN-NOISE_MEAN)
+    separationRange = np.linspace(MEAN-abs(sep),MEAN+abs(sep), 21)
+    fractionRange = np.linspace(.5, .99, 50)
+
+
+    #Plot 5 plots on figure
     fig = pl.figure()
-    sumStats, err, binCent, hist = fitSum(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, PA, fig, plot=1)
-    chi2=chiSq(binCent, hist, sumStats, fig)
+    #Fits to mixture of two Gaussians based on parameters specified above
+    fitParams, err, binCent, hist = fitMixture(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, DOMINANT_FLUX_FRACTION, fig, plot=1)
+    #Plots chi-squared distribution between mixture and fit
+    chi2=chiSq(binCent, hist, fitParams, fig)
     
-    var, anVar = varFit(MEAN, SIGMA, nmean, NOISE_SIGMA, PA, fig)
-    du, mu = meanFit(MEAN, SIGMA, nmean, NOISE_SIGMA, PA, fig)
-    propFit(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, p, fig)
+    #Varies separation and finds variance of fit
+    var, analyticVar = varySepFindVariance(MEAN, SIGMA, separationRange, NOISE_SIGMA, DOMINANT_FLUX_FRACTION, fig)
+    #Varies separation and finds mean of fit
+    separation, fitMean = varySepFindMean(MEAN, SIGMA, separationRange, NOISE_SIGMA, DOMINANT_FLUX_FRACTION, fig)
+    #Varies flux fraction and finds variance of fit
+    varyFluxFraction(MEAN, SIGMA, NOISE_MEAN, NOISE_SIGMA, fractionRange, fig)
     
     pl.show()
     
-    #index1 = np.argmin(chi2, axis=0)
-    #index2 = np.argmin(np.argmin(chi2, axis =0), axis=0)
-    #Min = chi2[index1][index2]
-    #print mini
-    #print Min
-    
-def chiSq(data, values, stats, fig):
+def chiSq(x, mixture, params, fig):
     #Make nxn grid
     n = 50
     chiGrid = np.zeros([n,n])
       
-    meanMin = stats[0]-stats[0]/5
-    meanMax = stats[0]+stats[0]/5
-    sigMin = stats[1]-stats[1]/5
-    sigMax = stats[1]+stats[1]/5
+    meanMin = params[0]-params[0]/5
+    meanMax = params[0]+params[0]/5
+    sigMin = params[1]-params[1]/5
+    sigMax = params[1]+params[1]/5
     for i in range(n):
         for j in range(n):
             mean = meanMin + (meanMax-meanMin)*float(i)/(n-1)
             sig = sigMin + (sigMax-sigMin)*float(j)/(n-1)
             chi = 0.0
-            for l in range(len(data)):
-                resid = (values[l] - gaussianpdf(data[l], mean, sig, stats[2]))#/np.sqrt(values[l])#Leave in sqrt's for ChiSq, else leastSq fit
-                chi = chi + resid**2
+            for bin in range(len(x)):
+                resid = (mixture[bin] - gaussianpdf(x[bin], mean, sig, params[2]))#/np.sqrt(mixture[bin])#Leave in sqrt's for ChiSq, else leastSq fit
+                chi += resid**2
             chiGrid[n-1-j,i] = chi
-            
+
     #Plot chiGrid
     ax = fig.add_subplot(235)
     ax = pl.imshow(chiGrid,extent=[meanMin, meanMax, sigMin, sigMax])
     pl.xlabel('Mean')
     pl.ylabel('Sigma')
     pl.colorbar(ax)
-    
-    #Get the index of the first min
-    index = np.unravel_index(np.argmin(chiGrid), chiGrid.shape)
-    #Plot the location of the bin
-    #mean = meanMin+(meanMax-meanMin)*float(index[1])/(n-1)
-    #sig = sigMin+(sigMax-sigMin)*float(n-1-index[0])/(n-1)
-    #pl.plot(mean, sig, 'wD')
-    #pl.xlim(meanMin, meanMax)
-    #pl.ylim(sigMin, sigMax)
-    #a = np.where(chiGrid.ravel()==chiGrid[index[0]][index[1]])    #Number of mins
-    #print 'There are ',len(a),'mins in this distr'
-    #print 'The min, ',chi2[index[0]][index[1]],', is at ',index
-    
+
     return chiGrid
         
-def propFit(u, s, nu, ns, p, fig):
+def varyFluxFraction(mean, sigma, contMean, contSigma, fluxFractions, fig):
     var = []
     varErr = []
     
     #Extract the variance and error for movement through pa
-    for pa in p:
-        stats, statErr, x, y = fitSum(u, s, nu, ns, pa, fig)
-        var.append(stats[1]**2)
+    for frac in fluxFractions:
+        fitParams, statErr, x, y = fitMixture(mean, sigma, contMean, contSigma, frac, fig)
+        var.append(fitParams[1]**2)
         varErr.append(statErr[1])
         
     #Plot mean vs. mean difference
     ax = fig.add_subplot(233)
-    ax.set_title('Variance vs. pa')
-    ax.errorbar(p,var, yerr=varErr)
-    ax.set_xlabel('pa')
+    ax.set_title('Variance vs. Flux Fraction')
+    ax.errorbar(fluxFractions,var, yerr=varErr)
+    ax.set_xlabel('Flux Fraction')
     ax.set_ylabel('Variance')
     return var, varErr
 
-def meanFit(mean, s, nmean, ns, pa, fig):
-    du = []
-    mu = []
-    muErr = []
+#Finds the mean of the fit for varying separation
+def varySepFindMean(mean, sigma, sepRange, contSigma, fluxFraction, fig):
+    sep = []
+    fitMean = []
+    fitMeanErr = []
     
     #Extract the mean and error for each movement of the mean of the first object
-    for u in nmean:
-        stats, statErr, x, y = fitSum(mean, s, u, ns, pa, fig)
-        du.append(u-mean)
-        mu.append(abs(stats[0]))
-        muErr.append(statErr[0])
+    for separation in sepRange:
+        fitParams, statErr, x, y = fitMixture(mean, sigma, separation, contSigma, fluxFraction, fig)
+        sep.append(separation-mean)
+        fitMean.append(abs(fitParams[0]))
+        fitMeanErr.append(statErr[0])
     
     #Plot mean vs. mean difference
     ax = fig.add_subplot(232)
     ax.set_title('Mean of Mixture vs. Delta Mean')
-    ax.errorbar(du, mu, yerr=muErr)
+    ax.errorbar(sep, fitMean, yerr=fitMeanErr)
     ax.set_xlabel('Delta Mean')
     ax.set_ylabel('Absolute Value of Mean of Fit')
-    ax.set_xlim(min(du)-1, max(du)+1)
-    return du, mu
+    ax.set_xlim(min(sep)-1, max(sep)+1)
+    return sep, fitMean
     
 #Plots the variance of the fit as a function of the difference in means
-def varFit(mean, s, nmean, ns, pa, fig):
-    du = []
+def varySepFindVariance(mean, sigma, sepRange, contSigma, fluxFrac, fig):
+    sep = []
     var = []
     varErr = []
     
     #Extract the variance and error for each movement of the mean of the first object
-    for u in nmean:
-        stats, statErr, x, y = fitSum(mean, s, u, ns, pa, fig)
-        du.append(u-mean)
-        var.append(stats[1]**2)
+    for separation in sepRange:
+        fitParams, statErr, x, y = fitMixture(mean, sigma, separation, contSigma, fluxFrac, fig)
+        sep.append(separation-mean)
+        var.append(fitParams[1]**2)
         varErr.append(statErr[1])
     #Calculate the analytic variance
-    anVar = analyticMixVar(s, ns, du)
+    analyticVar = analyticMixVariance(sigma, contSigma, sep)
     
     #Plot variance vs. mean difference
     ax = fig.add_subplot(231)
     ax.set_title('Variance vs. Delta Mean')
-    ax.errorbar(du, var, yerr=varErr, label = 'From Mixture')
-    ax.plot(du, anVar, 'k', label = 'Analytic Variance')
+    ax.errorbar(sep, var, yerr=varErr, label = 'From Mixture')
+    ax.plot(sep, analyticVar, 'k', label = 'Analytic Variance')
     ax.set_xlabel('Delta Mean')
     ax.set_ylabel('Variance')
-    ax.set_xlim(min(du)-1, max(du)+1)
+    ax.set_xlim(min(sep)-1, max(sep)+1)
     pl.legend(loc=9, prop={'size':9})
     
-    return var, anVar
+    return var, analyticVar
     
 #Formula for the variance of a 1D Gaussian Mixture
-def analyticMixVar(s, ns, du, pa=.5):
-    pb = 1 - pa
-    var = [pa*s**2 + pb*ns**2 + pa*pb*delta**2 for delta in du] 
-    return var
+def analyticMixVariance(domSigma, contSigma, separations, domFrac=.5):
+    contFrac = 1 - domFrac
+    variance = [domFrac*domSigma**2 + contFrac*contSigma**2 + domFrac*contFrac*sep**2 for sep in separations] 
+    return variance
     
 def gaussianpdf(x, u, s, amp=1):
     return amp*np.exp(-(x-u)**2/(2*s**2))#/(np.sqrt(2*np.pi))
     
-def fitSum(u, s, nu, ns, pa, fig, plot=0):
+def fitMixture(mean, sigma, contMean, contSigma, fluxFrac, fig, plot=0):
     samples = 100000
     bins = 200
-    pb = 1 - pa
-    dataSamples = pa*samples
-    objSamples = pb*samples
+    contFluxFrac = 1 - fluxFrac
+    dataSamples = fluxFrac*samples
+    objSamples = contFluxFrac*samples
     
     #Set up bin space
-    xMin = np.mean([u, nu]) - 15*s
-    xMax = np.mean([u, nu]) + 15*s
+    xMin = np.mean([mean, contMean]) - 15*sigma
+    xMax = np.mean([mean, contMean]) + 15*sigma
     x = np.linspace(xMin, xMax, bins)
     binSize=(xMax-xMin)/bins
     
     #Draw samples for data and for undetected object
     np.random.seed(0)
-    data = np.random.normal(u, s, dataSamples)
-    obj = np.random.normal(nu, ns, objSamples)
+    data = np.random.normal(mean, sigma, dataSamples)
+    obj = np.random.normal(contMean, contSigma, objSamples)
     binSum = np.concatenate((data, obj))
     
     #Create histograms for data and object
@@ -178,23 +175,23 @@ def fitSum(u, s, nu, ns, pa, fig, plot=0):
     sumHist[sumHist==0] = 1
 
     #Extract the paramaters of the sum fit
-    p=[pa*u+pb*nu, np.sqrt(analyticMixVar(s, ns, [u-nu], pa)[0]), max(sumHist)]
+    p=[fluxFrac*mean+contFluxFrac*contMean, np.sqrt(analyticMixVariance(sigma, contSigma, [mean-contMean], fluxFrac)[0]), max(sumHist)]
     sumStats, sumErr = opt.curve_fit(gaussianpdf, sumEdgCent, sumHist, p0=p)#, sigma = np.sqrt(sumHist), absolute_sigma=True)#Remove sigma and absolute sigma if not a ChiSq test
     perr = np.sqrt(np.diag(sumErr))
     #Create fit
     sumFit = gaussianpdf(sumEdgCent, sumStats[0], sumStats[1], sumStats[2])
-   
+
     if plot==1:
         #Check area of fit vs. area of sum
         sumArea = sum(binSize*sumHist)
         fitArea = sint.quad(gaussianpdf, -np.inf, np.inf,(sumStats[0], sumStats[1], sumStats[2]))
         #Title Strings
-        params = [u, s, nu, ns]
+        params = [mean, sigma, contMean, contSigma]
         statsFit = [sumStats[0], sumStats[1], sumStats[2]]
         paramStr = "Params (meanData, sigData, meanObj, sigObj): " + ', '.join([str(param) for param in params])
         statStr = 'Stats of Sum Fit(mean, sigma, amp): ' + ', '.join([str(np.round(stat, 2)) for stat in statsFit])
         areaStr = 'Areas (fit, hist wBinSize='+str(binSize)+'): '+str(round(fitArea[0],2))+', '+str(sumArea)
-        titleStr = 'Gaussian Mixture, '+ str(pa*100) + '% Red with '+str(pb*100)+'% Green\n' + paramStr + '\n' + statStr + '\n' + areaStr  
+        titleStr = 'Gaussian Mixture, '+ str(fluxFrac*100) + '% Red with '+str(contFluxFrac*100)+'% Green\n' + paramStr + '\n' + statStr + '\n' + areaStr  
         #Plot histograms
         ax = fig.add_subplot(234)
         ax.hist(binSum, sumEdgCent, color='b', label = 'Sum')
@@ -206,7 +203,6 @@ def fitSum(u, s, nu, ns, pa, fig, plot=0):
         ax.plot(sumEdgCent, sumFit, color='k', label = 'Sum Fit')
         ax.set_xlim(sumStats[0]-6*sumStats[1],sumStats[0]+6*sumStats[1])
         pl.legend(loc=2, prop={'size':7})
-
     return sumStats, perr, sumEdgCent, sumHist
     
 if __name__ == "__main__":
